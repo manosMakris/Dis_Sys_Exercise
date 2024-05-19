24fbc91ae1d16a87225853fa402ae2c775c03ba3
manosMakris <mnsmkrs@gmail.com>
2024-05-19 16:36:41 +0300

    Changed TestSecurityConfig.java file

--- src/main/java/gr/hua/distSys/backend/BusinessManagement/config/TestSecurityConfig.java
+++ src/main/java/gr/hua/distSys/backend/BusinessManagement/config/TestSecurityConfig.java
@@ -1,22 +1,30 @@
 package gr.hua.distSys.backend.BusinessManagement.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 @Profile("test")
-public class TestSecurityConfig extends WebSecurityConfigurerAdapter {
+public class TestSecurityConfig {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
             .csrf().disable() // Disable CSRF for simplicity in testing
             .authorizeRequests()
                 .antMatchers("/api/auth/signup", "/api/auth/signin").permitAll() // Allow access to the specific endpoints
                 .anyRequest().authenticated(); // Secure other endpoints
+        return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring().antMatchers("/api/auth/signup", "/api/auth/signin");
     }
 }
