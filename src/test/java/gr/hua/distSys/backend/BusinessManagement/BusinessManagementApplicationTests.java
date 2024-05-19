0a957ae490cd0085daca42b59411b9de8036c7e5
manosMakris <mnsmkrs@gmail.com>
2024-05-19 14:12:15 +0300

    Added tests in the project

--- src/test/java/gr/hua/distSys/backend/BusinessManagement/BusinessManagementApplicationTests.java
+++ src/test/java/gr/hua/distSys/backend/BusinessManagement/BusinessManagementApplicationTests.java
@@ -1,13 +1,57 @@
 package gr.hua.distSys.backend.BusinessManagement;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
-class BusinessManagementApplicationTests {
+@AutoConfigureMockMvc
+class SpringbootdemoApplicationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void testCreateUser() throws Exception {
+		// Arrange
+		String userJson = "{\"username\":\"apiuser\",\"email\":\"api@hua.gr\",\"password\":\"pass123\"}";
+
+
+		// Act
+		ResultActions result = mockMvc.perform(post("/api/auth/signup")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(userJson));
+
+		// Assert
+		result.andExpect(status().isOk());
+	}
+
+	@Test
+	public void testSignUser() throws Exception {
+		// Arrange
+		String userJson = "{\"username\":\"apiuser\",\"password\":\"pass123\"}";
+
+		// Act
+		ResultActions result = mockMvc.perform(post("/api/auth/signin")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(userJson));
+
+		// Assert
+		result.andExpect(status().isOk())
+				.andExpect(jsonPath("$.username").value("apiuser"));
+
 	}
 
 }
